1b5d2713e5a14bac07b64800aed50ccd162dad56
Ricky <doriflolo@gmail.com>
2022-04-24 22:38:40 -0400

    Prueba de commit

---
+++ src/sisinf/main.java
@@ -0,0 +1,12 @@
+package sisinf;
+
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
