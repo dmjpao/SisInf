723c94006dcb097da4442d46edcbf77d0e8c5ebe
mariela3810 <104334957+mariela3810@users.noreply.github.com>
2022-05-01 16:41:37 -0400

    Update registrar_cliente.java

--- cliente/registrar_cliente.java
+++ cliente/registrar_cliente.java
@@ -1,4 +1,17 @@
- public void insertar(String id, String nombre, String telefono, String fecha) {
+package controlador;
+
+import modelo.variables;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+public class consulta {
+    java.sql.Statement st;
+    ResultSet rs;
+    conectar conec=new conectar();
+    variables var = new variables();
+
+    public void insertar(String id, String nombre, String telefono, String fecha) {
         try {
             Connection conexion = conec.conectar();
             st = conexion.createStatement();
@@ -11,3 +24,4 @@
             JOptionPane.showMessageDialog(null, "El registro no se guardo " + e, "Mensaje", JOptionPane.ERROR_MESSAGE);
         }
     }
+
