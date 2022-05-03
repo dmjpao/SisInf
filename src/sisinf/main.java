a4cc9a56f6865a69a01ab3d29c3ff8efd4b077cd
Arieleandrovv <Arieleandrovv@outlook.com>
2022-05-02 21:16:46 -0400

    creacion de interfaz para verificacion de stocks

--- src/sisinf/main.java
+++ src/sisinf/main.java
@@ -1,6 +1,7 @@
 package sisinf;
 
 import Modelo.Conexion;
+import Vista.InterfazDetalleDeStock;
 import Vista.InterfazRegistrarNuevoProducto;
 
 public class main {
@@ -11,9 +12,13 @@ public class main {
     public static void main(String[] args) {
         //Conexion con = new Conexion();
         //con.ConectarDB();
-        InterfazRegistrarNuevoProducto ventana=new InterfazRegistrarNuevoProducto();
+        /*InterfazRegistrarNuevoProducto ventana=new InterfazRegistrarNuevoProducto();
         ventana.setVisible(true);
-        ventana.setLocationRelativeTo(null);
+        ventana.setLocationRelativeTo(null);*/
+        InterfazDetalleDeStock ventana2=new InterfazDetalleDeStock();
+        ventana2.setVisible(true);
+        ventana2.setLocationRelativeTo(null);
+        
     }
     
 }
