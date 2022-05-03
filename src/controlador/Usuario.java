7029e4dfcb094efef72262fbca2c050e6f8937d0
OliverPozo <80186282+OliverPozo@users.noreply.github.com>
2022-05-03 00:46:56 -0400

    Clase Usuario

---
+++ src/controlador/Usuario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author olive
+ */
+public class Usuario {
+    public String nombre;
+    public String contrasena;
+    public String fecNac;
+    public float salario;
+    public String sexo;
+    public String fecIni;
+    public String fecFin;
+    public String rol;
+    
+    public Usuario(){
+        
+    }
+}
